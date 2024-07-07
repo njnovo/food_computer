d601843db1f007abc9b5a2674376168e0285e2ab
ipadproiphone <66954909+ipadproiphone@users.noreply.github.com>
2024-07-07 16:33:56 -0600

    Add files via upload

--- ph_sensor.py
+++ ph_sensor.py
@@ -15,11 +15,25 @@ ads = ADS.ADS1115(i2c)
 # Create single-ended input on channel 0
 chan = AnalogIn(ads, ADS.P1)
 
-# Create differential input between channel 0 and 1
-# chan = AnalogIn(ads, ADS.P0, ADS.P1)
+# Replace with your actual offset value
+Offset = 0.0
 
-print("{:>5}\t{:>5}".format("raw", "v"))
+# Start an iterator thread so serial buffer doesn't overflow
+
+def read_ph_sensor():
+    buf = []  # buffer for reading analog values
+    for _ in range(10):  # Get 10 sample values from the sensor to smooth the value
+        buf.append(chan.voltage)
+        time.sleep(0.01)
+
+    # Sort the analog values from small to large
+    buf.sort()
+
+    avg_value = sum(buf[2:8]) / 6  # Take the average value of 6 center samples
+    ph_value = avg_value * 5.0 / 1024 / 6  # Convert the analog value into voltage
+    ph_value = 3.5 * ph_value + Offset  # Convert the voltage into pH value
+    return ph_value
 
 while True:
-    print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-    time.sleep(0.5)
+    ph_value = read_ph_sensor()
+    print(f"pH: {ph_value:.2f}")
